2b2a170f758baf2cfc094d965ce0af19ca630bbb
tchanee <johnnb98@gmail.com>
2021-11-21 22:31:07 +0100

    progress on face statistics :)

--- AverageFace report4/scripts/face_statistics.py
+++ AverageFace report4/scripts/face_statistics.py
@@ -8,16 +8,17 @@ from matplotlib import pyplot as plt
 p = "shape_predictor_68_face_landmarks.dat"
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor(p)
-path = '../output/'
+outputPath = '../output/'
+dbPath = '../faces/'
 
 
 def oklesgo():
-    for filePath in sorted(os.listdir(path)):
+    for filePath in sorted(os.listdir(outputPath)):
 
         if filePath.endswith(".jpg"):
-            print(path)
+            print(outputPath)
             print(filePath)
-            image = cv2.imread(os.path.join(path, filePath))
+            image = cv2.imread(os.path.join(outputPath, filePath))
             # convert to floating point?
             # image = np.float32(image) / 255.0
 
@@ -40,11 +41,12 @@ def oklesgo():
             landmarks2 = face_utils.shape_to_np(landmarks2)
             # print(len(landmarks2))
             # print(landmarks2)  # this seems more accurate!
-            newName = path + filePath + "G.txt"
+            newName = outputPath + filePath + "G.txt"
             print(newName)
             file_object = open(newName, "w")
             np.savetxt(file_object, landmarks2, fmt="%d", delimiter=" ")
             file_object.close()
+
 
 def read_output_txt(name):
     # landmarks = np.loadtxt(name, dtype=int, delimiter='\n')
@@ -59,15 +61,58 @@ def read_output_txt(name):
 
     return landmarks2
 
+
 def euclidean_distance(arr1, arr2):
-    return np.sqrt((arr1[0] - arr2[0])**2 + (arr1[1] - arr2[1])**2)
+    return np.sqrt((arr1[0] - arr2[0]) ** 2 + (arr1[1] - arr2[1]) ** 2)
 
-def find_variance():
-    return None
 
-if __name__ == '__main__':
-    # oklesgo()
+# Read points from text files in directory
+def readPoints(path):
+    # Create an array of array of points.
+    pointsArray = []
 
+    # List all files in the directory and read points from text files one by one
+    for filePath in sorted(os.listdir(path)):
+
+        if filePath.endswith("G.txt"):
+
+            # Create an array of points.
+            points = []
+
+            # Read points from filePath
+            with open(os.path.join(path, filePath)) as file:
+                for line in file:
+                    x, y = line.split()
+                    points.append((int(x), int(y)))
+
+            # Store array of points
+            pointsArray.append(points)
+
+    return pointsArray
+
+
+# Read all jpg images in folder.
+def readImages(path):
+    # Create array of array of images.
+    imagesArray = []
+
+    # List all files in the directory and read points from text files one by one
+    for filePath in sorted(os.listdir(path)):
+
+        if filePath.endswith(".jpg"):
+            # Read image found.
+            img = cv2.imread(os.path.join(path, filePath))
+
+            # Convert to floating point
+            img = np.float32(img) / 255.0
+
+            # Add to array of images
+            imagesArray.append(img)
+
+    return imagesArray
+
+
+def outputStats():
     landmarks1 = read_output_txt("../output/output1.jpg")
     landmarks2 = read_output_txt("../output/output2.jpg")
     landmarks3 = read_output_txt("../output/output3.jpg")
@@ -120,14 +165,12 @@ if __name__ == '__main__':
     middle_face_width2 = euclidean_distance(landmarks2[2], landmarks2[14])
     middle_face_width_array.append(middle_face_width2)
 
-
     # Face length
     left_eye_face_length2 = euclidean_distance(landmarks2[7], landmarks2[21])
     left_eye_face_length_array.append(left_eye_face_length2)
 
     right_eye_face_length2 = euclidean_distance(landmarks2[9], landmarks2[22])
     right_eye_face_length_array.append(right_eye_face_length2)
-
 
     # Mouth width && length
     mouth_width2 = euclidean_distance(landmarks2[48], landmarks2[54])
@@ -146,14 +189,12 @@ if __name__ == '__main__':
     middle_face_width3 = euclidean_distance(landmarks3[2], landmarks3[14])
     middle_face_width_array.append(middle_face_width3)
 
-
     # Face length
     left_eye_face_length3 = euclidean_distance(landmarks3[7], landmarks3[21])
     left_eye_face_length_array.append(left_eye_face_length3)
 
     right_eye_face_length3 = euclidean_distance(landmarks3[9], landmarks3[22])
     right_eye_face_length_array.append(right_eye_face_length3)
-
 
     # Mouth width && length
     mouth_width3 = euclidean_distance(landmarks3[48], landmarks3[54])
@@ -181,6 +222,81 @@ if __name__ == '__main__':
     mouth_length_var = np.var(mouth_length_array)
 
 
+def calculate1FaceStats(person, i, j):
+    # Some statistics that might be interesting:
+    # Face width: distance of points 6-12, 1-17, 3-15,
+    # Face length: 8-22 , 10-23
+    # Mouth width: 49-55
+    # Mouth length: 52-58
+    return euclidean_distance(person[i], person[j])
+
+
+def dbStats():
+    # Array of array of points (numImages * 68)
+    dbPoints = readPoints(dbPath)
+
+    images = readImages(dbPath)
+    numImages = len(images)
+
+    bottom_face_width_array = []
+    top_face_width_array = []
+    middle_face_width_array = []
+    left_eye_face_length_array = []
+    right_eye_face_length_array = []
+    mouth_width_array = []
+    mouth_length_array = []
+
+    for i in range(0, numImages):
+        person_i = dbPoints[i]
+        
+        top_face_width = calculate1FaceStats(person_i, 0, 16)
+        top_face_width_array.append(top_face_width)
+
+        middle_face_width = calculate1FaceStats(person_i, 2, 14)
+        middle_face_width_array.append(middle_face_width)
+
+        bottom_face_width = calculate1FaceStats(person_i, 5, 11)
+        bottom_face_width_array.append(bottom_face_width)
+
+        left_eye_face_length = calculate1FaceStats(person_i, 7, 21)
+        left_eye_face_length_array.append(left_eye_face_length)
+
+        right_eye_face_length = calculate1FaceStats(person_i, 9, 22)
+        right_eye_face_length_array.append(right_eye_face_length)
+
+        mouth_width = calculate1FaceStats(person_i, 48, 54)
+        mouth_width_array.append(mouth_width)
+
+        mouth_length = calculate1FaceStats(person_i, 51, 57)
+        mouth_length_array.append(mouth_length)
+
+    # Calculating the means
+    bottom_face_width_mean = np.mean(bottom_face_width_array)
+    top_face_width_mean = np.mean(top_face_width_array)
+    middle_face_width_mean = np.mean(middle_face_width_array)
+    left_eye_face_length_mean = np.mean(left_eye_face_length_array)
+    right_eye_face_length_mean = np.mean(right_eye_face_length_array)
+    mouth_width_mean = np.mean(mouth_width_array)
+    mouth_length_mean = np.mean(mouth_length_array)
+
+    # Calculating the variances
+    bottom_face_width_var = np.var(bottom_face_width_array)
+    top_face_width_var = np.var(top_face_width_array)
+    middle_face_width_var = np.var(middle_face_width_array)
+    left_eye_face_length_var = np.var(left_eye_face_length_array)
+    right_eye_face_length_var = np.var(right_eye_face_length_array)
+    mouth_width_var = np.var(mouth_width_array)
+    mouth_length_var = np.var(mouth_length_array)
+
+def distance_ratio(mean, new):
+    return abs(mean - new) / mean
+
+
+if __name__ == '__main__':
+    oklesgo()
+    outputStats()
+    dbStats()
+
     # Zucc's turn
     zuccmark = read_output_txt("../output/zucc.jpg")
 
@@ -193,10 +309,5 @@ if __name__ == '__main__':
     # Mouth width && length
     zucc_mouth_width = euclidean_distance(zuccmark[48], zuccmark[54])
     zucc_mouth_length = euclidean_distance(zuccmark[51], zuccmark[57])
-
-    # Plotting the means
-
-
-    # Plotting the vars
 
 
